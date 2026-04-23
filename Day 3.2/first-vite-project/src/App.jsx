5cc19c679bb334c78f421bddbbcc5bee0936ac1d
raghavdolyar <raghavdolyardell@gmail.com>
2026-04-23 16:46:11 +0000

    completed with day 3.2

---
+++ Day 3.2/first-vite-project/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <h2>Hello Coders</h2>
+    );
+}
+
+// only one function can be default exported from a single .jsx file
+// otherwise only named exports are allowed then from that same .jsx file
